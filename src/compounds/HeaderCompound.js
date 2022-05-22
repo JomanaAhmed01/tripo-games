414de2d5ac7637b64289b54d4a1b17ec15990589
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-05-22 11:13:19 +0200

    Editing the space of the Navbra & the Logo

--- src/compounds/HeaderCompound.js
+++ src/compounds/HeaderCompound.js
@@ -2,13 +2,17 @@ import React from 'react';
 import styled from "styled-components"
 import HeaderLogo from '../components/Header/HeaderLogo';
 import HeaderNavbar from '../components/Header/HeaderNavbar';
+import HeaderText from '../components/Header/HeaderText';
 import img from '../img/bg.jpg'
 
 function HeaderCompound() {
   return (
     <Wrapper>
-      <HeaderLogo />
-      <HeaderNavbar />
+      <HeaderWrapper>
+        <HeaderLogo />
+        <HeaderNavbar />
+      </HeaderWrapper>
+      <HeaderText />
     </Wrapper>
   );
 }
@@ -17,8 +21,18 @@ function HeaderCompound() {
 export const Wrapper = styled.div`
   border: 3px solid red;
   background-image: url(${img});
+`
+
+export const HeaderWrapper = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
+  padding-left: 100px;
+  padding-right: 100px;
+
+  @media screen and (max-width: 1024px) {
+    padding-left: 30px;
+    padding-right: 30px;
+  }
 `
 
 export default HeaderCompound;
