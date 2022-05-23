7ad0839b6b1212d61f76d6f0dc7d2990aebed809
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-05-23 10:56:50 +0200

    Adding GameCardsCompound.js to the App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import HeaderCompound from './compounds/HeaderCompound';
 import './App.css';
+import HeaderCompound from './compounds/HeaderCompound';
+import GameCardsCompound from './compounds/GameCardsCompound';
 
 function App() {
   return (
     <div className="App">
       <HeaderCompound />
+      <GameCardsCompound />
     </div>
   );
 }
