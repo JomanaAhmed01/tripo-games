99c26b455dedbb01044861043dd336ea28d22c56
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-05-23 10:53:40 +0200

    Renaming CardsCompound.js to GameCardsCompound.js

---
+++ src/compounds/GameCardsCompound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components"
+
+function GameCardsCompound() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default GameCardsCompound;
