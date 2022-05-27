87cf8b6b07fa6eb1f302ad012065e9cde0abb4b7
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-05-27 17:02:05 +0200

    Creating Tripo-games prject's Games cards

--- src/compounds/GameCardsCompound.js
+++ src/compounds/GameCardsCompound.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styled from "styled-components"
+import GamesCards from '../components/GameCards/GamesCards'
 
 function GameCardsCompound() {
   return (
     <Wrapper>
-      
+      <GamesCards />
     </Wrapper>
   );
 }
-
 
 export const Wrapper = styled.div`
   
