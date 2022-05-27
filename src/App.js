0d869a790648026eb23853a7df36ef1caaa48a67
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-05-27 17:26:43 +0200

    Adding AboutCompound to the App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import HeaderCompound from './compounds/HeaderCompound';
 import GameCardsCompound from './compounds/GameCardsCompound';
+import AboutCompound from './compounds/AboutCompound'
 
 function App() {
   return (
     <div className="App">
       <HeaderCompound />
       <GameCardsCompound />
+      <AboutCompound />
     </div>
   );
 }
