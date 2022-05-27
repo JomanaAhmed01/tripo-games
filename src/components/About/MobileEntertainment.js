178e39370314706877224fecde596d2bfe06b833
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-05-27 17:39:56 +0200

    Creating MobileEntertainment file

---
+++ src/components/About/MobileEntertainment.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from "styled-components"
+
+function MobileEntertainment() {
+    return (
+        <Wrapper>
+          <Header>About Tripo</Header>
+        </Wrapper>
+    );
+}
+
+export const Wrapper = styled.div`
+
+`
+
+export const Header = styled.h3`
+  border: 3px solid red;
+  text-align: center;
+  color: #272443;
+
+  @media screen and (max-width: 768px) {
+    font-size: 15px;
+  }
+`
+
+export default MobileEntertainment;
