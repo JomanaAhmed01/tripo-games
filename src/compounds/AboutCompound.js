e5f9a5110c47fe68b350a7a62215691862b61755
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-05-27 17:27:30 +0200

    Adding AboutTripo.js file to the AboutCompound.js file

---
+++ src/compounds/AboutCompound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components"
+import AboutTripo from '../components/About/AboutTripo'
+
+function AboutCompound() {
+    return (
+        <Wrapper>
+          <AboutTripo />
+        </Wrapper>
+    );
+}
+
+export const Wrapper = styled.div`
+
+`
+
+export default AboutCompound;
