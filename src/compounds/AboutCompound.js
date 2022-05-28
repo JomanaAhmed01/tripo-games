234c6dd5178b9f13ce258aec9916924c47d75cce
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-05-28 09:09:31 +0200

    Defining MobileEntertainment file in the AboutCompound

--- src/compounds/AboutCompound.js
+++ src/compounds/AboutCompound.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from "styled-components"
 import AboutTripo from '../components/About/AboutTripo'
+import MobileEntertainment from '../components/About/MobileEntertainment';
 
 function AboutCompound() {
     return (
         <Wrapper>
           <AboutTripo />
+          <MobileEntertainment />
         </Wrapper>
     );
 }
