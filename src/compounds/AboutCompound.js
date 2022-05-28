cd78ec79d6ed31c50f6afc9fb4db49bf4e211389
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-05-28 09:15:17 +0200

    Defining OurGames.js file in the AboutCompound

--- src/compounds/AboutCompound.js
+++ src/compounds/AboutCompound.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from "styled-components"
 import AboutTripo from '../components/About/AboutTripo'
 import MobileEntertainment from '../components/About/MobileEntertainment';
+import OurGames from '../components/About/OurGames'
 
 function AboutCompound() {
     return (
         <Wrapper>
           <AboutTripo />
           <MobileEntertainment />
+          <OurGames />
         </Wrapper>
     );
 }
