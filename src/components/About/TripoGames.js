bc5ca8de5a224b684ebfa2a220bd27610d79e2f2
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-05-28 09:37:00 +0200

    Ceating TripoGames file

---
+++ src/components/About/TripoGames.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components"
+
+function AboutTripo() {
+    return (
+        <Wrapper>
+            
+        </Wrapper>
+    );
+}
+
+export const Wrapper = styled.div`
+  background-image: url('./img/games-bg.png');
+  height: 600px;
+`
+
+export default AboutTripo;
