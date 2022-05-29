b7f72991fd168e0ec86c36d04098229bd35a1827
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-05-29 19:41:36 +0200

    Changing the padding-bottom from 50px to 300px

--- src/components/contact/JoinTeam.js
+++ src/components/contact/JoinTeam.js
@@ -27,7 +27,7 @@ export const Wrapper = styled.div`
   margin-top: 200px;
   padding-left: 100px;
   padding-top: 50px;
-  padding-bottom: 50px;
+  padding-bottom: 300px;
 `
 
 export const Text = styled.p`
