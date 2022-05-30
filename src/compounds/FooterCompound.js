6532e9b147303cb1005c3a168a0705192c06bbe8
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-05-30 14:39:27 +0200

    Defining ImmersiveGaming.js file in the FooterCompound.js file

--- src/compounds/FooterCompound.js
+++ src/compounds/FooterCompound.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from "styled-components"
+import ImmersiveGaming from '../components/Footer/ImmersiveGaming'
 
 function FooterCompound() {
     return (
         <Wrapper>
-          
+          <ImmersiveGaming />
         </Wrapper>
     );
 }
