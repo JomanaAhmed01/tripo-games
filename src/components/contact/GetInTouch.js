2a5661364ab5dc1838044a37c7ed3de47b87e633
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-05-30 09:49:54 +0200

    Creating GetInTouch file

---
+++ src/components/contact/GetInTouch.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components";
+
+function JoinTeam() {
+    return (
+        <Wrapper>
+xxxxxxxxxxxxxxxx
+        </Wrapper>
+    );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default JoinTeam;
