29788fc0cc3a9b9b1881309e0c05d760723aad6a
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-05-30 12:42:41 +0200

    Create SubNewsLetter file

---
+++ src/components/contact/SubNewsLetter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from "styled-components";
+
+function SubNewsLetter() {
+    return (
+        <Wrapper>
+ddddddddddddddddd
+        </Wrapper>
+    );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+export default SubNewsLetter;
