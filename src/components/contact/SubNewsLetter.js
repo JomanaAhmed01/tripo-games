31cf9caad89f06d17b905f15ef1996d2ffab8bd7
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-05-30 14:14:18 +0200

    Creating Tripo-games prject's Subscribe to our newsletter section

--- src/components/contact/SubNewsLetter.js
+++ src/components/contact/SubNewsLetter.js
@@ -4,13 +4,99 @@ import styled from "styled-components";
 function SubNewsLetter() {
     return (
         <Wrapper>
-ddddddddddddddddd
+            <Header>Subscribe to Our Newsletter</Header>
+            <EmailFormWrapper>
+                <EmailLabel>Email *</EmailLabel>
+                <EmailForm type='text' />
+                <JoinBtn>Join</JoinBtn>
+            </EmailFormWrapper>
         </Wrapper>
     );
 }
 
 
 export const Wrapper = styled.div`
-  
+  background-color: #DA392B;
+  width: 388px;
+  height: 210px;
+  border-radius: 15px;
+  padding-left: 25px;
+  margin-right: 100px;
+  margin-bottom: 20px;
+  margin-top: 230px;
+
+  @media screen and (max-width: 1180px) {
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  @media screen and (max-width: 530px) {
+    width: 80%;
+    height: 270px;
+  }
 `
+
+export const Header = styled.p`
+  color: #FFFFFF;
+  font-size: 21px;
+  font-family: 'Roboto', sans-serif;
+  font-weight: bold;
+
+  @media screen and (max-width: 530px) {
+    font-size: 19px;
+  }
+`
+
+export const EmailFormWrapper = styled.div`
+  @media screen and (max-width: 530px) {
+    display: flex;
+    flex-direction: column;
+  }
+`
+
+export const EmailLabel = styled.label`
+  display: block;
+  color: #FFFFFF;
+  margin-bottom: 10px;
+  font-size: 16px;
+
+  @media screen and (max-width: 530px) {
+    font-size: 18px;
+  }
+`
+
+export const EmailForm = styled.input`
+  width: 238px;
+  height: 59px;
+  border-radius: 15px;
+  border-color: transparent;
+  margin-right: 20px;
+  padding-left: 15px;
+  font-size: 18px;
+
+  :focus {
+    outline: none;
+  }
+
+  @media screen and (max-width: 530px) {
+    width: 90%;
+    margin-bottom: 20px;
+  }
+`
+
+export const JoinBtn = styled.button`
+  background-color: #FCFF72;
+  border-color: transparent;
+  border-radius: 10px;
+  width: 79px;
+  height: 60px;
+  color: #272443;
+  cursor: pointer !important;
+
+  @media screen and (max-width: 530px) {
+    width: 94%;
+    font-size: 18px;
+  }
+`
+
 export default SubNewsLetter;
