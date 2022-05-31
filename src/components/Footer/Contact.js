d527a03f359a190d92eddd15e5f7286a0cf5c20a
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-05-31 11:15:55 +0200

    Creating Contact.js file

---
+++ src/components/Footer/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components"
+
+function Contact() {
+    return (
+        <Wrapper>
+         gggggggggggg 
+        </Wrapper>
+    );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default Contact;
