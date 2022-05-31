72a85dc344ab2d82091cda1c7269d1909e3270c2
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-05-31 11:23:50 +0200

    Creating Tripo-games prject's contact section in the Footer section

--- src/components/Footer/Contact.js
+++ src/components/Footer/Contact.js
@@ -4,7 +4,14 @@ import styled from "styled-components"
 function Contact() {
     return (
         <Wrapper>
-         gggggggggggg 
+          <Header>Contact</Header>
+
+          <ElementsWrapper>
+            <Element>Info@mysite.com</Element>
+            <Element>Tel: 123-456-7890</Element>  
+            <Element>500 Terry Francois St</Element>
+            <Element>San Francisco, CA 94158</Element>
+          </ElementsWrapper>
         </Wrapper>
     );
 }
@@ -12,6 +19,23 @@ function Contact() {
 
 export const Wrapper = styled.div`
   
+`
+
+export const Header = styled.p`
+  color: #272443;
+  font-size: 21px;
+  font-weight: bold;
+  font-family: 'Roboto', sans-serif;
+`
+
+export const ElementsWrapper = styled.div`
+  
+`
+
+export const Element = styled.p`
+  color: #757575;
+  font-size: 16px;
+  font-family: 'Roboto', sans-serif;
 `
 
 export default Contact;
