31447cac9542586ba0c6400bbcc9f5515eb798bf
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-06-01 10:11:20 +0200

    Creating GamesCompound.js file

---
+++ src/compounds/GamesCompound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from "styled-components"
+import KiponHeader from '../components/KiponGame/KiponHeader'
+
+function GamesCompound() {
+  return (
+    <Wrapper>
+       <KiponHeader />
+    </Wrapper>
+  );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default GamesCompound;
