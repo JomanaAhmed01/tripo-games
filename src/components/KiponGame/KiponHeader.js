06caf9081bdc86218eee625b1ff6226bc5208a31
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-06-01 10:11:06 +0200

    Creating Tripo-games Project's Kipon game's Header section

---
+++ src/components/KiponGame/KiponHeader.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from "styled-components"
+import HeaderLogo from '../Header/HeaderLogo'
+import HeaderNavbar from '../Header/HeaderNavbar'
+
+function KiponHeader() {
+    return (
+        <Wrapper>
+            <HeaderLogo />
+            <HeaderNavbar />
+        </Wrapper>
+    );
+}
+
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-left: 100px;
+  padding-right: 100px;
+
+  @media screen and (max-width: 1024px) {
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+`
+
+export default KiponHeader;
