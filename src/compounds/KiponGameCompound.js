d7da48a11bc46584f65cc984b7bdcc451f5ca9c6
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-06-01 12:44:03 +0200

    Defining KiponGameCard.js file in the KiponGameCompound.js file & Wrapping the KiponGameCard.js file in a single wrapper

--- src/compounds/KiponGameCompound.js
+++ src/compounds/KiponGameCompound.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import styled from "styled-components"
 import KiponHeader from '../components/KiponGame/KiponHeader'
+import KiponGameCard from '../components/KiponGame/KiponGameCard'
 
 function KiponGameCompound() {
   return (
     <Wrapper>
       <KiponHeader />
+      <CardWrapper>
+        <KiponGameCard />
+      </CardWrapper>
     </Wrapper>
   );
 }
@@ -13,6 +17,17 @@ function KiponGameCompound() {
 
 export const Wrapper = styled.div`
   
+`
+
+export const CardWrapper = styled.div`
+  background-image: url('./img/kipon-game-bg-one.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+
+  @media screen and (max-width: 880px) {
+    background-image: none;
+  }
 `
 
 export default KiponGameCompound;
