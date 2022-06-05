3ff2a28c437db9ff844fa9164ed20bbbe595d105
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-06-05 13:25:08 +0200

    Creating Tripo-games project's Treasure box's game card section

--- src/compounds/TreasureBoxGameCompound.js
+++ src/compounds/TreasureBoxGameCompound.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import styled from "styled-components"
 import TreasureBoxHeader from '../components/TreasureBoxGame/TreasureBoxHeader'
+import TreasureBoxCardWrapper from '../components/TreasureBoxGame/TreasureBoxGameCard'
 
 function TreasureBoxGameCompound() {
-  return (
-    <Wrapper>
-      <TreasureBoxHeader />
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <TreasureBoxHeader />
+            <CardWrapper>
+                <TreasureBoxCardWrapper />
+            </CardWrapper>
+        </Wrapper>
+    );
 }
 
 
 export const Wrapper = styled.div`
   
+`
+
+export const CardWrapper = styled.div`
+  background-image: url('./img/treasure-box-bg-one.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+
+  @media screen and (max-width: 880px) {
+    background-image: none;
+  }
 `
 
 export default TreasureBoxGameCompound;
