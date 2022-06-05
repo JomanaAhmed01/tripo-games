070979a6e89314a994346d16f580556dcc28cbdc
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-06-05 15:14:04 +0200

    Creating CareersCompound.js file

---
+++ src/compounds/CareersCompound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components"
+import CareersHeader from '../components/Careers/CareersHeader'
+
+function CareersCompound() {
+    return (
+        <Wrapper>
+          <CareersHeader />
+        </Wrapper>
+    );
+}
+
+export const Wrapper = styled.div`
+
+`
+
+export default CareersCompound;
