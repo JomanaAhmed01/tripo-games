64a2b3d99f9f05e5103a7589efd08db61cf7e7d3
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-06-05 13:22:00 +0200

    Creating Tripo-games project's Treasure box's game header section

--- src/compounds/TreasureBoxGameCompound.js
+++ src/compounds/TreasureBoxGameCompound.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from "styled-components"
+import TreasureBoxHeader from '../components/TreasureBoxGame/TreasureBoxHeader'
 
 function TreasureBoxGameCompound() {
   return (
     <Wrapper>
-      cccccccccccccccccc
+      <TreasureBoxHeader />
     </Wrapper>
   );
 }
