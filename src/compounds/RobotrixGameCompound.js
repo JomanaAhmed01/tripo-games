5fa70e9442c6a4ab34c27ea45b39520e8ff1d6b3
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-06-05 12:08:07 +0200

    Adding Robotrix Game card section & screenshots section to the RobotrixGameCompound.js file

--- src/compounds/RobotrixGameCompound.js
+++ src/compounds/RobotrixGameCompound.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import styled from "styled-components"
 import RobotrixHeader from '../components/RobotrixGame/RobotrixHeader.js'
+import RobotrixGameCard from '../components/RobotrixGame/RobotrixGameCard.js'
+import RobotrixGameScreenshots from '../components/RobotrixGame/RobotrixGameScreenshots'
 
 function RobotrixGameCompound() {
   return (
     <Wrapper>
       <RobotrixHeader />
+      <CardWrapper>
+        <RobotrixGameCard />
+      </CardWrapper>
+      <RobotrixGameScreenshots />
     </Wrapper>
   );
 }
@@ -13,6 +19,17 @@ function RobotrixGameCompound() {
 
 export const Wrapper = styled.div`
   
+`
+
+export const CardWrapper = styled.div`
+  background-image: url('./img/robotrix-game-bg-one.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+
+  @media screen and (max-width: 880px) {
+    background-image: none;
+  }
 `
 
 export default RobotrixGameCompound;
