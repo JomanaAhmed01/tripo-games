02a1ffeb0ac74647a43129cbb658f847911e83a9
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-06-05 12:31:02 +0200

    Defining all the new files in the RobotrixGameCompound.js file

--- src/compounds/RobotrixGameCompound.js
+++ src/compounds/RobotrixGameCompound.js
@@ -4,6 +4,9 @@ import RobotrixHeader from '../components/RobotrixGame/RobotrixHeader.js'
 import RobotrixGameCard from '../components/RobotrixGame/RobotrixGameCard.js'
 import RobotrixGameScreenshots from '../components/RobotrixGame/RobotrixGameScreenshots'
 import RobotrixRating from '../components/RobotrixGame/RobotrixRating.js'
+import RobotrixPlayability from '../components/RobotrixGame/RobotrixPlayability.js'
+import RobotrixPlayNow from '../components/RobotrixGame/RobotrixPlayNow.js'
+import RobotrixFooter from '../components/RobotrixGame/RobotrixFooter.js'
 
 function RobotrixGameCompound() {
   return (
@@ -14,6 +17,9 @@ function RobotrixGameCompound() {
       </CardWrapper>
       <RobotrixGameScreenshots />
       <RobotrixRating />
+      <RobotrixPlayability />
+      <RobotrixPlayNow />
+      <RobotrixFooter />
     </Wrapper>
   );
 }
