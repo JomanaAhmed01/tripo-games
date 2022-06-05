c01f8f1cd24963ace342c8b7684d245696e9f16e
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-06-05 12:30:41 +0200

    Creating Tripo-games project's Robotrix's game footer section

---
+++ src/components/RobotrixGame/RobotrixFooter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from "styled-components"
+import FooterCompound from '../../compounds/FooterCompound'
+
+function RobotrixFooter() {
+    return (
+        <Wrapper>
+            <FooterCompound />
+        </Wrapper>
+    );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default RobotrixFooter;
