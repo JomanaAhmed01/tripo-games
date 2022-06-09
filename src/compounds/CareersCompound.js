59b07db41374a3661208fffd149a5589e87f31ff
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-06-09 11:44:30 +0200

    Defining CareersHiring.js file in the CareersCompound.js

--- src/compounds/CareersCompound.js
+++ src/compounds/CareersCompound.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from "styled-components"
 import CareersHeader from '../components/Careers/CareersHeader'
+import CareersHiring from '../components/Careers/CareersHiring'
 
 function CareersCompound() {
     return (
         <Wrapper>
           <CareersHeader />
+          <CareersHiring />
         </Wrapper>
     );
 }
