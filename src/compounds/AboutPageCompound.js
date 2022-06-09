fe3548baf1ded36bcce51f279cae65d55b576f28
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-06-09 17:34:48 +0200

    Creating Tripo-games project's about page compound

---
+++ src/compounds/AboutPageCompound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from "styled-components"
+
+function AboutPageCompound() {
+    return (
+        <Wrapper>
+          dddddddddddddddddddddd
+        </Wrapper>
+    );
+}
+
+export const Wrapper = styled.div`
+
+`
+
+export default AboutPageCompound;
