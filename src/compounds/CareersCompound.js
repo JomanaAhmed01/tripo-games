efba468098ceec2ad456630427fa2c8d50ea5d58
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-06-09 14:08:31 +0200

    Defining the CareersJobOpenings.js file in the CareersCompound.js file

--- src/compounds/CareersCompound.js
+++ src/compounds/CareersCompound.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from "styled-components"
 import CareersHeader from '../components/Careers/CareersHeader'
 import CareersHiring from '../components/Careers/CareersHiring'
+import CareersJobOpenings from '../components/Careers/CareersJobOpenings'
 
 function CareersCompound() {
     return (
         <Wrapper>
           <CareersHeader />
           <CareersHiring />
+          <CareersJobOpenings />
         </Wrapper>
     );
 }
