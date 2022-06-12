5a75273b1da0537d19417682f7c1fd5b30397dff
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-06-12 18:07:24 +0200

    Defining AboutCreatingGame.js file in the AboutPageCompound,js

--- src/compounds/AboutPageCompound.js
+++ src/compounds/AboutPageCompound.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from "styled-components"
 import AboutHeader from '../components/AboutPage/AboutHeader'
 import AboutDesc from '../components/AboutPage/AboutDesc'
+import AboutCreatingGame from '../components/AboutPage/AboutCreatingGame'
 
 function AboutPageCompound() {
     return (
         <Wrapper>
           <AboutHeader />
           <AboutDesc />
+          <AboutCreatingGame />
         </Wrapper>
     );
 }
