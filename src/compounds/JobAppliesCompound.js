ccf2a42c0ec3eee65df59657df837de46fc09c61
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-06-19 18:01:34 +0200

    Defining GameDesigner.js file in the JobAppliesCompound.js file

--- src/compounds/JobAppliesCompound.js
+++ src/compounds/JobAppliesCompound.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styled from "styled-components"
 // import CommunityManager from '../components/JobApplies/CommunityManager'
-import QATester from '../components/JobApplies/QATester'
+// import QATester from '../components/JobApplies/QATester'
+import GameDesigner from '../components/JobApplies/GameDesigner';
 
 function JobAppliesCompound() {
   return (
     <Wrapper>
       {/* <CommunityManager /> */}
-      <QATester />
+      {/* <QATester /> */}
+      <GameDesigner />
     </Wrapper>
   );
 }
