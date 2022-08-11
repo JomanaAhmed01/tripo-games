9a00accd459d57d6d2bd8aaa27b645fa5dfffcff
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-11 12:38:35 +0200

    Adjusting the code

--- src/components/JobApplies/CommunityManager.js
+++ src/components/JobApplies/CommunityManager.js
@@ -4,44 +4,44 @@ import HeaderLogo from '../Header/HeaderLogo'
 import HeaderNavbar from '../Header/HeaderNavbar'
 import FooterCompound from '../../compounds/FooterCompound'
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500;700&family=Quicksand:wght@300&family=Roboto:wght@300&family=Suez+One&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500;700&family=Quicksand:wght@300&family=Roboto:wght@300&family=Suez+One&display=swap');
 </style>
 
 function CommunityManager() {
-    return (
-        <Wrapper>
-            <HeaderWrapper>
-                <HeaderLogo />
-                <HeaderNavbar />
-            </HeaderWrapper>
+  return (
+    <Wrapper>
+      <HeaderWrapper>
+        <HeaderLogo />
+        <HeaderNavbar />
+      </HeaderWrapper>
 
-            <CommManager>
-                <HeaderText>Careers</HeaderText>
-                <Header>Community Manager</Header>
-                <Location>Location: San Francisco</Location>
-                <Employ>Employment Type: Contract</Employ>
-                <Button>Apply Now</Button>
-            </CommManager>
+      <CommManager>
+        <HeaderText>Careers</HeaderText>
+        <Header>Community Manager</Header>
+        <Location>Location: San Francisco</Location>
+        <Employ>Employment Type: Contract</Employ>
+        <Button>Apply Now</Button>
+      </CommManager>
 
-            <DescWrapper>
-                <TextWrapperOne>
-                    <TextHeaderOne>What You’ll Do</TextHeaderOne>
-                    <TextOne>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.</TextOne>
-                    <TextOne>This is a great space to write long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide. Tell your visitors the story of how you came up with the idea for your business and what makes you different from your competitors. Make your company stand out and show your visitors who you are. </TextOne>
-                    <TextOne>At Wix we’re passionate about making templates that allow you to build fabulous websites and it’s all thanks to the support and feedback from users like you! Keep up to date with New Releases and what’s Coming Soon in Wixellaneous in Support. Feel free to tell us what you think and give us feedback in the Wix Forum. If you’d like to benefit from a professional designer’s touch, head to the Wix Arena and connect with one of our Wix Pro designers. Or if you need more help you can simply type your questions into the Support Forum and get instant answers. To keep up to date with everything Wix, including tips and things we think are cool, just head to the Wix Blog!</TextOne>
-                </TextWrapperOne>
+      <DescWrapper>
+        <TextWrapperOne>
+          <TextHeaderOne>What You’ll Do</TextHeaderOne>
+          <TextOne>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.</TextOne>
+          <TextOne>This is a great space to write long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide. Tell your visitors the story of how you came up with the idea for your business and what makes you different from your competitors. Make your company stand out and show your visitors who you are. </TextOne>
+          <TextOne>At Wix we’re passionate about making templates that allow you to build fabulous websites and it’s all thanks to the support and feedback from users like you! Keep up to date with New Releases and what’s Coming Soon in Wixellaneous in Support. Feel free to tell us what you think and give us feedback in the Wix Forum. If you’d like to benefit from a professional designer’s touch, head to the Wix Arena and connect with one of our Wix Pro designers. Or if you need more help you can simply type your questions into the Support Forum and get instant answers. To keep up to date with everything Wix, including tips and things we think are cool, just head to the Wix Blog!</TextOne>
+        </TextWrapperOne>
 
-                <TextWrapperTwo>
-                    <TextHeaderTwo>Who You Are</TextHeaderTwo>
-                    <TextTwo>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.</TextTwo>
-                    <TextTwo>This is a great space to write long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide. Tell your visitors the story of how you came up with the idea for your business and what makes you different from your competitors. Make your company stand out and show your visitors who you are. </TextTwo>
-                    <TextTwo>At Wix we’re passionate about making templates that allow you to build fabulous websites and it’s all thanks to the support and feedback from users like you! Keep up to date with New Releases and what’s Coming Soon in Wixellaneous in Support. Feel free to tell us what you think and give us feedback in the Wix Forum. If you’d like to benefit from a professional designer’s touch, head to the Wix Arena and connect with one of our Wix Pro designers. Or if you need more help you can simply type your questions into the Support Forum and get instant answers. To keep up to date with everything Wix, including tips and things we think are cool, just head to the Wix Blog!</TextTwo>
-                </TextWrapperTwo>
-            </DescWrapper>
-            
-            <FooterCompound />
-        </Wrapper>
-    );
+        <TextWrapperTwo>
+          <TextHeaderTwo>Who You Are</TextHeaderTwo>
+          <TextTwo>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.</TextTwo>
+          <TextTwo>This is a great space to write long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide. Tell your visitors the story of how you came up with the idea for your business and what makes you different from your competitors. Make your company stand out and show your visitors who you are. </TextTwo>
+          <TextTwo>At Wix we’re passionate about making templates that allow you to build fabulous websites and it’s all thanks to the support and feedback from users like you! Keep up to date with New Releases and what’s Coming Soon in Wixellaneous in Support. Feel free to tell us what you think and give us feedback in the Wix Forum. If you’d like to benefit from a professional designer’s touch, head to the Wix Arena and connect with one of our Wix Pro designers. Or if you need more help you can simply type your questions into the Support Forum and get instant answers. To keep up to date with everything Wix, including tips and things we think are cool, just head to the Wix Blog!</TextTwo>
+        </TextWrapperTwo>
+      </DescWrapper>
+
+      <FooterCompound />
+    </Wrapper>
+  );
 }
 
 
