f62ce4bb154a75a0523fbad290df842df1818caa
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-11 13:23:09 +0200

    Defining Compound files in the App.js file & Adding it to the Router link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import React from 'react';
-import HeaderCompound from './compounds/HeaderCompound';
-import GameCardsCompound from './compounds/GameCardsCompound';
-import AboutCompound from './compounds/AboutCompound';
-import ContactCompound from './compounds/ContactCompound';
-import FooterCompound from './compounds/FooterCompound';
+import HeaderCompound from './compounds/HeaderCompound'
+import GameCardsCompound from './compounds/GameCardsCompound'
+import AboutCompound from './compounds/AboutCompound'
+import ContactCompound from './compounds/ContactCompound'
+import FooterCompound from './compounds/FooterCompound'
 import KiponGameCompound from './compounds/KiponGameCompound'
 import RobotrixGameCompound from './compounds/RobotrixGameCompound'
 import TreasureBoxGameCompound from './compounds/TreasureBoxGameCompound'
 import CareersCompound from './compounds/CareersCompound'
 import AboutPageCompound from './compounds/AboutPageCompound'
 import CommunityManagerCompound from './compounds/CommunityManagerCompound'
+import QATesterCompound from './compounds/QATesterCompound'
+import GameDesignerCompound from './compounds/GameDesignerCompound'
+import GameDeveloperCompound from './compounds/GameDeveloperCompound'
 
 function App() {
   return (
@@ -47,6 +50,18 @@ function App() {
 
         <Route path="/CommunityManagerCompound">
           <CommunityManagerCompound />
+        </Route>
+
+        <Route path="/QATesterCompound">
+          <QATesterCompound />
+        </Route>
+
+        <Route path="/GameDesignerCompound">
+          <GameDesignerCompound />
+        </Route>
+
+        <Route path="/GameDeveloperCompound">
+          <GameDeveloperCompound />
         </Route>
       </Switch>
     </Router>
