287e4f84a3750ddb4d83e9c1cd73accaf25b4183
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-11 13:15:54 +0200

    Creating CommunityManager Compound.js file

---
+++ src/compounds/CommunityManagerCompound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from "styled-components"
+import CommunityManager from '../components/JobApplies/CommunityManager'
+
+function CommunityManagerCompound() {
+  return (
+    <Wrapper>
+      <CommunityManager />
+    </Wrapper>
+  );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default CommunityManagerCompound;
