0d9001820bf5ae0d85fc082349ea93183cceb62e
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-11 13:22:31 +0200

    Creating GameDeveloperCompound.js file

---
+++ src/compounds/GameDeveloperCompound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from "styled-components"
+import GameDeveloper from '../components/JobApplies/GameDeveloper'
+
+function GameDeveloperCompound() {
+  return (
+    <Wrapper>
+      <GameDeveloper />
+    </Wrapper>
+  );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default GameDeveloperCompound;
