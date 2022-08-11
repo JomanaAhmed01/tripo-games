4a3320da7754f3b1f2f0807315a3e15c8b3916f7
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-11 13:22:14 +0200

    Creating GameDesignerCompound.js file

---
+++ src/compounds/GameDesignerCompound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from "styled-components"
+import GameDesigner from '../components/JobApplies/GameDesigner'
+
+function GameDesignerCompound() {
+  return (
+    <Wrapper>
+      <GameDesigner />
+    </Wrapper>
+  );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default GameDesignerCompound;
