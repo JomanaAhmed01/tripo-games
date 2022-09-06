3b2c4937bb44135b38ebc2b1905d2bfc496fb791
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-06 21:27:11 +0200

    Writing the IDs as Numbers not Text

--- src/Data/Games.js
+++ src/Data/Games.js
@@ -1,23 +1,23 @@
 export const games = [
     {
-        id: '1',
+        id: 1,
         name: "kipon",
         title: 'Kipon',
         type: 'Casual - Free',
         Description: "I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you",
     },
     {
-        id: '2',
+        id: 2,
         name: "robotrix",
         title: 'Robotrix',
         type: 'RPG - Free',
         Description: "I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you",
     },
-    {
-        id: '3',
-        name: "treasure-box",
-        title: 'Treasure Box',
-        type: 'Adventure - Free',
-        Description: "I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you",
-    },
+    // {
+    //     id: 2,
+    //     name: "treasure-box",
+    //     title: 'Treasure Box',
+    //     type: 'Adventure - Free',
+    //     Description: "I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you",
+    // },
 ]
