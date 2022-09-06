22aef59f18b6f15a495e21e97ec12013f337fbdd
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-06 19:59:22 +0200

    Adding Games' Data to the Games.js file

--- src/Data/Games.js
+++ src/Data/Games.js
@@ -1,12 +1,20 @@
 export const games = [
     {
-      name: "fight-club",
-      title: "Fight Club",
-      genre: "drama",
-      folder: "films",
-      smallImage: "/images/films/drama/fight-club/small.jpg",
-      subTitle:
-        "Discontented with his capitalistic lifestyle, a white-collared insomniac forms an underground fight club with Tyler, a careless soap salesman. The project soon spirals down into something sinister.",
+        name: "kipon",
+        title: 'Kipon',
+        type: 'Casual - Free',
+        Description: "I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you",
     },
-  ]
-  
+    {
+        name: "robotrix",
+        title: 'Robotrix',
+        type: 'RPG - Free',
+        Description: "I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you",
+    },
+    {
+        name: "treasure-box",
+        title: 'Treasure Box',
+        type: 'Adventure - Free',
+        Description: "I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you",
+    },
+]
