b460c22c57f4da679e30d1bf79351472369a1a7a
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-06 21:39:17 +0200

    Removing commented code

--- src/Data/Games.js
+++ src/Data/Games.js
@@ -13,11 +13,4 @@ export const games = [
         type: 'RPG - Free',
         Description: "I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you",
     },
-    // {
-    //     id: 2,
-    //     name: "treasure-box",
-    //     title: 'Treasure Box',
-    //     type: 'Adventure - Free',
-    //     Description: "I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you",
-    // },
 ]
