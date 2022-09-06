73650d6385d1279fa982c8d08061d0e681e30e39
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-06 20:56:31 +0200

    Adding Data to the reviews.js file

---
+++ src/Data/reviews.js
@@ -0,0 +1,20 @@
+export const reviews = [
+    {
+        id: '1',
+        name: "first-review",
+        review: '“I"m a testimonial. Click to edit me and add text that says something nice about you and your services.”',
+        writer: "Wang Wai",
+    },
+    {
+        id: '2',
+        name: "second-review",
+        review: '“I"m a testimonial. Click to edit me and add text that says something nice about you and your services. Let your customers review you and tell their friends how great you are.”',
+        writer: "Jose Pablo",
+    },
+    {
+        id: '3',
+        name: "third-review",
+        review: '“I"m a testimonial. Click to edit me and add text that says something nice about you and your services.”',
+        writer: "Priya Gunga",
+    },
+]
