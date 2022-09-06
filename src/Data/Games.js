66aabc1fc3c0d560de3eaa00cb1d73197068ced3
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-06 19:50:59 +0200

    Creating Games.js file

---
+++ src/Data/Games.js
@@ -0,0 +1,12 @@
+export const games = [
+    {
+      name: "fight-club",
+      title: "Fight Club",
+      genre: "drama",
+      folder: "films",
+      smallImage: "/images/films/drama/fight-club/small.jpg",
+      subTitle:
+        "Discontented with his capitalistic lifestyle, a white-collared insomniac forms an underground fight club with Tyler, a careless soap salesman. The project soon spirals down into something sinister.",
+    },
+  ]
+  
