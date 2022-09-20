eec26d03856886968df4dbd4afbc4afd28839c31
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-20 14:09:35 +0200

    Adding Navbar section to the Contact page

--- src/compounds/ContactCompound.js
+++ src/compounds/ContactCompound.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from "styled-components"
+import HeaderLogo from '../components/Header/HeaderLogo'
+import HeaderNavbar from '../components/Header/HeaderNavbar'
 import JoinTeam from '../components/contact/JoinTeam'
 import GetInTouch from '../components/contact/GetInTouch'
 import SubNewsLetter from '../components/contact/SubNewsLetter'
@@ -7,6 +9,10 @@ import SubNewsLetter from '../components/contact/SubNewsLetter'
 function ContactCompound() {
   return (
     <Wrapper>
+      <HeaderWrapper>
+        <HeaderLogo />
+        <HeaderNavbar />
+      </HeaderWrapper>
       <JoinTeam />
       <ContactWrapper>
         <GetInTouch />
@@ -17,6 +23,19 @@ function ContactCompound() {
 }
 
 export const Wrapper = styled.div`
+  
+`
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-left: 100px;
+  padding-right: 100px;
+
+  @media screen and (max-width: 1024px) {
+    padding-left: 0px;
+    padding-right: 0px;
+  }
 `
 
 export const ContactWrapper = styled.div`
