ae928fa97f773ef0d56fc80c1a54bb5145980e77
Jomana ahmed <ojy.ahmed@outlook.com>
2025-08-14 13:44:14 +0200

    Aligning the code in a neat way

--- src/compounds/AboutCompound.js
+++ src/compounds/AboutCompound.js
@@ -1,23 +1,21 @@
-import React from 'react';
+import React from "react"
 import styled from "styled-components"
-import AboutTripo from '../components/About/AboutTripo'
-import MobileEntertainment from '../components/About/MobileEntertainment';
-import OurGames from '../components/About/OurGames'
-import TripoGames from '../components/About/TripoGames'
+import AboutTripo from "../components/About/AboutTripo"
+import MobileEntertainment from "../components/About/MobileEntertainment"
+import OurGames from "../components/About/OurGames"
+import TripoGames from "../components/About/TripoGames"
 
 function AboutCompound() {
-    return (
-        <Wrapper>
-          <AboutTripo />
-          <MobileEntertainment />
-          <OurGames />
-          <TripoGames />
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <AboutTripo />
+      <MobileEntertainment />
+      <OurGames />
+      <TripoGames />
+    </Wrapper>
+  )
 }
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.div``
 
-`
-
-export default AboutCompound;
+export default AboutCompound
