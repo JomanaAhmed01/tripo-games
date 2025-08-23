97dfe144a8cd814042901eb922437a6fa180ff9f
Jomana ahmed <ojy.ahmed@outlook.com>
2025-08-23 21:19:54 +0200

    Activating the Navbar in Home page

--- src/compounds/HeaderCompound.js
+++ src/compounds/HeaderCompound.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 import HeaderLogo from "../components/Header/HeaderLogo"
-// import HeaderNavbar from '../components/Header/HeaderNavbar';
+import HeaderNavbar from '../components/Header/HeaderNavbar';
 import HeaderText from "../components/Header/HeaderText"
 import HeaderApps from "../components/Header/HeaderApps"
 
@@ -10,7 +10,7 @@ function HeaderCompound() {
     <Wrapper>
       <HeaderWrapper>
         <HeaderLogo />
-        {/* <HeaderNavbar /> */}
+        <HeaderNavbar />
       </HeaderWrapper>
       <HeaderText />
       <HeaderApps />
