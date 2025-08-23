e590486b5da5bbdaaa1ac01d919eeb9ec0b1b3c3
Jomana ahmed <ojy.ahmed@outlook.com>
2025-08-23 23:08:44 +0200

    Fixing some bugs in the footer

--- src/components/Footer/FindUsOn.js
+++ src/components/Footer/FindUsOn.js
@@ -1,35 +1,32 @@
-import React from 'react';
+import React from "react"
 import styled from "styled-components"
 
 function FindUsOn() {
-    return (
-        <Wrapper>
-          <Header>Find Us On</Header>
+  return (
+    <Wrapper>
+      <Header>Find Us On</Header>
 
-          <ElementsWrapper>
-            <Image src='./img/app-store.png' />
-            <Image src='./img/google-play.png' />
-          </ElementsWrapper>
-        </Wrapper>
-    );
+      <ElementsWrapper>
+        <Image src="./img/app-store.png" />
+        <Image src="./img/google-play.png" />
+      </ElementsWrapper>
+    </Wrapper>
+  )
 }
 
-
-export const Wrapper = styled.div`
-  
-`
+export const Wrapper = styled.div``
 
 export const Header = styled.p`
   color: #272443;
   font-size: 21px;
   font-weight: bold;
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
 `
 
 export const ElementsWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 80%;
+  width: 325px;
 
   @media screen and (max-width: 930px) {
     justify-content: space-between;
@@ -44,4 +41,4 @@ export const Image = styled.img`
   margin-right: 20px;
 `
 
-export default FindUsOn;
+export default FindUsOn
